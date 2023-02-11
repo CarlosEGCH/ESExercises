7c7bc13c52f7ef26c4667978595ee032bebe3165
CarlosEGCH <carlitosgch4@gmail.com>
2023-02-11 18:51:31 +0000

    Up to Exc 3 done

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 class MyApp
 {
 
+    // Exercise 1:
+
     static int fibonacci(int n)
     {
 
@@ -23,12 +25,62 @@ class MyApp
         }
     }
 
+    // Exercise 2:
+
+    static double leibniz(int n)
+    {
+        double res = 0.0;
+        bool add = true;
+
+        if (n == 0) return 0.0;
+
+        for (double i = 1.0; i < n; i = i + 2.0)
+        {
+
+            res = add ? res + (4.0 / (i)) : res - (4.0 / (i));
+
+            add = !add;
+        }
+
+        return res;
+
+    }
+
+    // Exercise 3:
+
+    static void readBigger()
+    {
+
+        System.Console.Write("Type a number: ");
+
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.Write("Type another number: ");
+
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        if (firstNumber == secondNumber) System.Console.WriteLine("Both numbers are equal.");
+        else if (firstNumber > secondNumber) System.Console.WriteLine(firstNumber + " is bigger.");
+        else System.Console.WriteLine(secondNumber + " is bigger.");
+
+    }
+
     static void Main()
     {
 
         // Exercise 1: Print the first 20 Fibonacci numbers
 
-        printFibonacci(20);
+        // printFibonacci(20);
+
+
+        // Exercise 2: Print the Nth iteration of Leibniz's Pi Formula
+
+        // System.Console.WriteLine(leibniz(200));
+
+        // Exercise 3: Read 2 numbers and print out the biggest
+
+        // readBigger();
+
     }
 
 }
